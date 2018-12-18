89d522b408c2d516d09bccd0284fa4572a690c1d
Hadi Nur Wahid <hadiykk99@gmail.com>
2018-12-18 08:56:22 +0700

    almost 100

--- routes/tutorial.js
+++ routes/tutorial.js
@@ -2,11 +2,12 @@ const router = require('express').Router();
 const {create,read,update} = require('../controllers/tutorialController');
 const delet = require('../controllers/tutorialController').delete;
 const {tutorial} = require('../middlewares/packager');
+const {isLogin} = require('../middlewares/')
 
-router.post('/',tutorial,create);
-router.get('/:id',read);
-router.get('/',read);
-router.put('/:id',tutorial,update);
-router.delete('/:id',delet);
+router.post('/', isLogin,tutorial,create);
+router.get('/:id', isLogin,read);
+router.get('/', isLogin,read);
+router.put('/:id', isLogin,tutorial,update);
+router.delete('/:id', isLogin,delet);
 
 module.exports = router;
--- test/difficulty.test.js
+++ test/difficulty.test.js
@@ -10,34 +10,12 @@ const app = require('../app');
 const clearUsersCollection = require('../helpers/clearUsersCollection');
 
 let difficultyId = null
+let accessToken
 
 chai.use(chaiHttp);
 
 describe('Difficulty', () => {
   before((done) => {
-      User
-          .create({
-        name: 'Hadi nw',
-        email: 'hadinw@mail.com',
-        password: 'secreet'
-      })
-      .then(user => {
-        User
-            .findOne({
-              email: 'hadinw@mail.com'
-            })
-            .then(userFound => {
-              let user = {
-                id: userFound._id,
-                name: userFound.name,
-                email: userFound.email
-              }
-              token = jwt.sign(user, process.env.JWT_SECRET)
-              userId = String(user.id)   
-              done()
-            })            
-      })
-
       Difficulty.create({
         title: 'easy',
         description: 'easy drawing',
@@ -45,8 +23,24 @@ describe('Difficulty', () => {
       })
       .then(created => {
         difficultyId = String(created._id)
+        done()
       })
   })
+
+  before(async function () {
+    const user = { email: 'hadi@mail.com', password: 'secret' };
+    await chai
+            .request(app)
+            .post('/users/register')
+            .send(user);
+
+    const response = await chai
+                            .request(app)
+                            .post('/users/login')
+                            .send(user);
+
+    accessToken = response.body.accessToken;
+});
 
   after((done) => {
     User.deleteMany({}, () => {
@@ -56,92 +50,201 @@ describe('Difficulty', () => {
     })
   })
 
+  describe('POST /difficulty', () => {
+    it('POST / difficulty should return created difficulty and status 201', (done) => {
+      let obj = {
+        title: 'hard',
+        description: 'this drawing for hard',
+        thumbnail : 'thumbnail',  
+      }
+      chai.request(app)
+          .post('/difficulties')
+          .send(obj)
+          .set('Access-Token', accessToken)
+          .end((err, result) => {
+            expect(result).to.have.status(201)
+            expect(result.body).to.have.property('title')
+            expect(result.body).to.have.property('description')
+            expect(result.body).to.have.property('thumbnail')
+            expect(result).to.have.property('status')
+            done()
+          })
+    })
 
-  it('POST / difficulty should return created difficulty and status 201', (done) => {
+    it("should send an error object with a message and a 400 status code (no token)", async function () {
+      let obj = {
+        title: 'hard',
+        description: 'this drawing for hard',
+        thumbnail : 'thumbnail',  
+      }
+      const response = await chai
+                            .request(app)
+                            .post("/difficulties")
+                            .send(obj);
+      expect(response).to.have.status(400);
+      expect(response.body).to.be.an("object");
+      expect(response.body).to.have.property('errors');
+      expect(response.body.errors).to.have.property('token');
+      expect(response.body.errors.token.message).to.equal('Please provide your access token');
+  });
+
+  it('should send an error object with a message and a 400 status code (no title key)', async function () {
     let obj = {
-      title: 'hard',
       description: 'this drawing for hard',
       thumbnail : 'thumbnail',  
     }
-    chai.request(app)
-        .post('/difficulties')
-        .send(obj)
-        .end((err, result) => {
-          expect(result).to.have.status(201)
-          expect(result.body).to.have.property('title')
-          expect(result.body).to.have.property('description')
-          expect(result.body).to.have.property('thumbnail')
-          expect(result).to.have.property('status')
-          done()
-        })
-  })
 
-  // it('POST /tutorial should return error with status code 400 if the title is empty', (done) => {
-  //   let toturialObj = {
-  //     description: 'this is the tutorial for drawing',
-  //     thumbnail : '',
-  //     difficulty : '5c0bd50510b1a50f3bfb7ffd'    
-  //   }
-  //   chai.request(app)
-  //       .post('/tutorials')
-  //       .send(toturialObj)
-  //       .end((err, result) => {
-  //         expect(result).to.have.status(400)
-  //         // expect(result.body).to.have.property('title')
-  //         // expect(result.body).to.have.property('description')
-  //         // expect(result.body).to.have.property('thumbnail')
-  //         // expect(result.body).to.have.property('difficulty')
-  //         // expect(result).to.have.property('status')
-  //         done()
-  //       })
-  // })
+    const response = await chai
+                            .request(app)
+                            .post("/difficulties")
+                            .set('Access-Token', accessToken)
+                            .send(obj);
 
-  it(' GET /difficulties should return all difficulty and should have status 200', (done) => {
-    chai.request(app)
-        .get('/difficulties')
-        .end((err, result) => {
-          expect(result).to.have.property('status')
-          expect(result).to.have.status(200)
-          expect(result.body).to.be.an('array')
-          expect(result.body).to.have.lengthOf(2)
+    expect(response).to.have.status(400);
+    expect(response.body).to.be.an("object");
+    expect(response.body).to.have.property('errors')
+    expect(response.body.errors.create.message).to.equal('Create difficulty error');
+    expect(response.body.errors.create.error.errors.title.message).to.equal('title is required');
+});
 
-          expect(result.body[0]).to.have.property('title')
-          expect(result.body[0]).to.have.property('description')
-          expect(result.body[0]).to.have.property('thumbnail')
-          done()
-        })
-  })
+it('should send an error object with a message and a 400 status code (no description key)', async function () {
+  let obj = {
+    title: 'title',
+    thumbnail : 'thumbnail'
+  }
 
-  it('PUT /difficulties/:id should return updated difficulty ', (done) => {
+  const response = await chai
+                          .request(app)
+                          .post("/difficulties")
+                          .set('Access-Token', accessToken)
+                          .send(obj);
+
+  expect(response).to.have.status(400);
+  expect(response.body).to.be.an("object");
+  expect(response.body).to.have.property('errors')
+  expect(response.body.errors.create.message).to.equal('Create difficulty error');
+  expect(response.body.errors.create.error.errors.description.message).to.equal('description is required');
+});
+
+
+})
+
+  describe('GET /difficulties', () => {
+    it('GET /difficulties should return all difficulty and should have status 200', (done) => {
+      chai.request(app)
+          .get('/difficulties')
+          .set('Access-Token', accessToken)
+          .end((err, result) => {
+            expect(result).to.have.property('status')
+            expect(result).to.have.status(200)
+            expect(result.body).to.be.an('array')
+            expect(result.body[0]).to.have.property('title')
+            expect(result.body[0]).to.have.property('description')
+            expect(result.body[0]).to.have.property('thumbnail')
+            done()
+          })
+    })
+
+
+    it('GET /difficulties should return one detail difficulty and should have status 200', (done) => {
+      chai.request(app)
+          .get('/difficulties/'+difficultyId)
+          .set('Access-Token', accessToken)
+          .end((err, result) => {
+            expect(result).to.have.property('status')
+            expect(result).to.have.status(200)
+            expect(result.body).to.be.an('object')
+            expect(result.body).to.have.property('title')
+            expect(result.body).to.have.property('description')
+            expect(result.body).to.have.property('thumbnail')
+            done()
+          })
+    })
+
+    it('GET /difficulties should return error and  status 400 if the id is invalid objectId', (done) => {
+      chai.request(app)
+          .get('/difficulties/invalidhoho')
+          .set('Access-Token', accessToken)
+          .end((err, result) => {
+            expect(result).to.have.property('status')
+            expect(result).to.have.status(400)
+            expect(result.body).to.be.an('object')
+            expect(result.body).to.have.property('errors')
+            done()
+          })
+    })
+
+    it("should send an error object with a message and a 400 status code (no token)", async function () {
+      
+      const response = await chai
+                            .request(app)
+                            .get('/difficulties')
+      expect(response).to.have.status(400);
+      expect(response.body).to.be.an("object");
+      expect(response.body).to.have.property('errors');
+      expect(response.body.errors).to.have.property('token');
+      expect(response.body.errors.token.message).to.equal('Please provide your access token');
+  });
+
+})
+  
+    it('PUT /difficulties/:id should return updated difficulty ', (done) => {
+      let obj = {
+        title: 'title edited',
+        description: 'description edited',
+        thumbnail : 'thumbnail edited'  
+      }
+      chai.request(app)
+          .put('/difficulties/'+difficultyId)
+          .send(obj)
+          .set('Access-Token', accessToken)
+          .end((err, result) => {
+            if (err) {
+              console.log(err)
+            }
+            expect(result).to.have.status(200)
+            expect(result.body).to.have.property('title')
+            expect(result.body).to.have.property('description')
+            expect(result.body).to.have.property('thumbnail')
+            done()
+          })         
+    })
+
+    it("should send an error object with a message and a 400 status code (no token)", async function () {
+      
+      const response = await chai
+                            .request(app)
+                            .put('/difficulties/'+difficultyId)
+      expect(response).to.have.status(400);
+      expect(response.body).to.be.an("object");
+      expect(response.body).to.have.property('errors');
+      expect(response.body.errors).to.have.property('token');
+      expect(response.body.errors.token.message).to.equal('Please provide your access token');
+  });
+
+  it('PUT /difficulties should return error and should have status 400 if one of property not provided', (done) => {
     let obj = {
-      title: 'title edited',
       description: 'description edited',
       thumbnail : 'thumbnail edited'  
     }
     chai.request(app)
-        .put('/difficulties/'+difficultyId)
+        .get('/difficulties/'+difficultyId)
+        .set('Access-Token', accessToken)
         .send(obj)
         .end((err, result) => {
-          if (err) {
-            console.log(err)
-          }
-          // console.log(result.body, '=====')
+          expect(result).to.have.property('status')
           expect(result).to.have.status(200)
-          expect(result.body).to.have.property('title')
-          expect(result.body).to.have.property('description')
-          expect(result.body).to.have.property('thumbnail')
-        //   expect(result.body.title).to.equal(obj.title)
-        //   expect(result.body.description).to.equal(obj.description)
-        //   expect(result.body.thumbnail).to.equal(thumbnail)
-        //   expect(result.body.difficulty).to.equal(obj.difficulty)
+          expect(result.body).to.be.an('object')
+          done()
         })
-
-        done()
   })
-
+    
+  
+ describe('DELETE /difficulties', () => {
   it('DELETE /difficulties/:id should return deleted difficulty and status 200 ', (done) => {
     chai.request(app)
         .delete('/difficulties/'+difficultyId)
+        .set('Access-Token', accessToken)
         .end((err, result) => {
           if (err) {
             console.log('testing delete difficulty error', err)
@@ -151,9 +254,22 @@ describe('Difficulty', () => {
           expect(result.body).to.have.property('description')
           expect(result.body).to.have.property('thumbnail')
         })
-
         done()
   })
+
+  it("should send an error object with a message and a 400 status code (no token)", async function () {
+      
+    const response = await chai
+                          .request(app)
+                          .delete('/difficulties/'+difficultyId)
+    expect(response).to.have.status(400);
+    expect(response.body).to.be.an("object");
+    expect(response.body).to.have.property('errors');
+    expect(response.body.errors).to.have.property('token');
+    expect(response.body.errors.token.message).to.equal('Please provide your access token');
+  });
+})
+ 
 
 
 })
